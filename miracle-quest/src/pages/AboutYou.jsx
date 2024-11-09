e8ee61d98b0c8d70a43c3f3f17f7e3298abb1c89
AshleyCW-pers <alienvspred2013@gmail.com>
2024-11-09 12:54:37 +0800

    Commiting rio time

--- miracle-quest/src/pages/AboutYou.jsx
+++ miracle-quest/src/pages/AboutYou.jsx
@@ -72,7 +72,7 @@ const AboutYou = () => {
         const options = {
             month: 'short',
             day: 'numeric',
-            timeZone: 'America/Halifax', // Use Halifax timezone
+            timeZone: 'America/Rio_Branco', // Use Rio Branco timezone
         };
     
         switch (timeRange) {
@@ -81,7 +81,7 @@ const AboutYou = () => {
             case 'month':
                 return date.toLocaleDateString('en-US', options);
             case 'year':
-                return date.toLocaleDateString('en-US', { month: 'short', year: '2-digit', timeZone: 'America/Halifax' });
+                return date.toLocaleDateString('en-US', { month: 'short', year: '2-digit', timeZone: 'America/Rio_Branco' });
             default:
                 return date.toLocaleDateString('en-US', options);
         }
@@ -104,7 +104,7 @@ const AboutYou = () => {
         <>
             <Navigation />
             <Container className="py-4">
-                <h2 className="text-center mb-4">Nutrition Trends</h2>
+                <h2 className="text-center mb-4">Nutrition Trend</h2>
 
                 {error && (
                     <Alert variant="danger" className="mb-4">
